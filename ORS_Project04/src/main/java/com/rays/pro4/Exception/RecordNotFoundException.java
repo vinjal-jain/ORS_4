c439c84e8de4dd394bf071018f27feeb9c2916a5
vinjal-jain <vinjaljain14it@gmail.com>
2025-02-12 12:02:56 +0530

    done

---
+++ ORS_Project04/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rays.pro4.Exception;
+
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author VINJAL JAIN
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg) {
+		super(msg);
+	}
+}
